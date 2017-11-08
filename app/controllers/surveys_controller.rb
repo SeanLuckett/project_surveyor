fbee8640f13997a24833bb9c3b4a27dd01d3fd92
Sean Luckett <whitecrane007@gmail.com>
2017-11-08 15:06:26 -0700

    Adds form for building a survey: title and description

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,4 +2,8 @@ class SurveysController < ApplicationController
   def index
     render :index, locals: { surveys: Survey.all }
   end
+
+  def new
+    render :new, locals: { survey: Survey.new }
+  end
 end

bb286a311b5686caf5ea3137a791bedecd2df162
Sean Luckett <whitecrane007@gmail.com>
2017-11-09 08:54:57 -0700

    Adds back to home button for all pages but root path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_home_button
+    unless request.fullpath == '/'
+      '<div class="row">
+         <div class="col-sm-12"><a href="/">Back to Home</a></div>
+       </div>'.html_safe
+    end
+  end
 end
